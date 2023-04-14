09fbebcfee01853081a53d7bac24cc2a5934c6d5
Max <bespalov.m.9@ya.ru>
2023-04-14 21:37:54 +0300

    intens for settings screen icons were added

--- app/src/main/java/bes/max/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/bes/max/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package bes.max.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +15,42 @@ class SettingsActivity : AppCompatActivity() {
         backIcon.setOnClickListener {
             finish()
         }
+
+        val shareIcon = findViewById<ImageView>(R.id.settings_activity_icon_share)
+        shareIcon.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.link_for_app_share))
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        val supportIcon = findViewById<ImageView>(R.id.settings_activity_icon_support)
+        supportIcon.setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_for_support)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_theme_for_support))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text_for_support))
+            }
+            //if (intent.resolveActivity(packageManager) != null)
+                startActivity(intent)
+
+        }
+
+        val agreementIcon = findViewById<ImageView>(R.id.settings_activity_icon_agreement)
+        agreementIcon.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(getString(R.string.link_for_app_share))
+            )
+            //if (intent.resolveActivity(packageManager) != null)
+                startActivity(intent)
+        }
+
     }
 
 }
