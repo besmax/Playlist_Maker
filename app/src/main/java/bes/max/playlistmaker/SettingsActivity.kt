4c838551441ebcd20f71fe59b5ca2cc42cc20155
Max <bespalov.m.9@ya.ru>
2023-04-15 21:16:07 +0300

    intents refactoring in settings activity, androidManifest was populated with queries for checking intents before starts

--- app/src/main/java/bes/max/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/bes/max/playlistmaker/SettingsActivity.kt
@@ -12,45 +12,49 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backIcon = findViewById<ImageView>(R.id.back_icon)
-        backIcon.setOnClickListener {
-            finish()
-        }
+        backIcon.setOnClickListener { finish() }
 
         val shareIcon = findViewById<ImageView>(R.id.settings_activity_icon_share)
         shareIcon.setOnClickListener {
-            val sendIntent: Intent = Intent().apply {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_TEXT, getString(R.string.link_for_app_share))
-                type = "text/plain"
-            }
-            val shareIntent = Intent.createChooser(sendIntent, null)
-            startActivity(shareIntent)
+            startActivity(shareAppLinkIntent())
         }
 
         val supportIcon = findViewById<ImageView>(R.id.settings_activity_icon_support)
         supportIcon.setOnClickListener {
-            val intent = Intent().apply {
-                action = Intent.ACTION_SENDTO
-                data = Uri.parse("mailto:")
-                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_for_support)))
-                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_theme_for_support))
-                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text_for_support))
-            }
-            //if (intent.resolveActivity(packageManager) != null)
-                startActivity(intent)
-
+            val sendEmailIntent = sendEmailIntent()
+            if (sendEmailIntent.resolveActivity(packageManager) != null)
+                startActivity(sendEmailIntent)
         }
 
         val agreementIcon = findViewById<ImageView>(R.id.settings_activity_icon_agreement)
         agreementIcon.setOnClickListener {
-            val intent = Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse(getString(R.string.link_for_app_share))
-            )
-            //if (intent.resolveActivity(packageManager) != null)
-                startActivity(intent)
+            val openAgreementIntent = openUserAgreementIntent()
+            if (openAgreementIntent.resolveActivity(packageManager) != null) {
+                startActivity(openAgreementIntent)
+            }
         }
-
     }
+
+    private fun shareAppLinkIntent(): Intent {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.link_for_app_share))
+            type = "text/plain"
+        }
+        return Intent.createChooser(sendIntent, null)
+    }
+
+    private fun sendEmailIntent(): Intent = Intent().apply {
+        action = Intent.ACTION_SENDTO
+        data = Uri.parse("mailto:")
+        putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_for_support)))
+        putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_theme_for_support))
+        putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text_for_support))
+    }
+
+    private fun openUserAgreementIntent(): Intent = Intent(
+        Intent.ACTION_VIEW,
+        Uri.parse(getString(R.string.link_for_app_share))
+    )
 
 }
