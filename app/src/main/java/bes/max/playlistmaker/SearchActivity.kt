162502f20463f408114322258252cb0e41849299
Max <bespalov.m.9@ya.ru>
2023-04-16 18:56:22 +0300

    input from edit text was saved in bundle and restored after in SearchActivity (unnecessary since it happening automatically)

--- app/src/main/java/bes/max/playlistmaker/SearchActivity.kt
+++ app/src/main/java/bes/max/playlistmaker/SearchActivity.kt
@@ -2,17 +2,27 @@ package bes.max.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.ImageView
 import com.google.android.material.textfield.TextInputEditText
 
 class SearchActivity : AppCompatActivity() {
+
+    private var savedSearchInputText = ""
+
+    companion object {
+        const val SEARCH_INPUT_TEXT = "SEARCH_INPUT_TEXT"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val searchEditText = findViewById<TextInputEditText>(R.id.search_activity_edit_text)
+        searchEditText.setText(savedSearchInputText)
+
         val backIcon = findViewById<ImageView>(R.id.search_activity_back_icon)
 
         backIcon.setOnClickListener {
@@ -21,17 +31,26 @@ class SearchActivity : AppCompatActivity() {
 
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
 
             override fun afterTextChanged(s: Editable?) {
+                savedSearchInputText = s.toString()
             }
-
         }
 
         searchEditText.addTextChangedListener(textWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString(SEARCH_INPUT_TEXT, savedSearchInputText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getString(SEARCH_INPUT_TEXT)
     }
 }
