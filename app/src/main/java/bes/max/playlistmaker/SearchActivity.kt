2761c7b6fa95f9fb928aed11e2cc87a76c626ceb
Max <bespalov.m.9@ya.ru>
2023-04-16 16:27:09 +0300

    activity_search layout was populated, SearchActivity was populated with empty text watcher and click listener for back icon

--- app/src/main/java/bes/max/playlistmaker/SearchActivity.kt
+++ app/src/main/java/bes/max/playlistmaker/SearchActivity.kt
@@ -2,10 +2,36 @@ package bes.max.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.ImageView
+import com.google.android.material.textfield.TextInputEditText
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchEditText = findViewById<TextInputEditText>(R.id.search_activity_edit_text)
+        val backIcon = findViewById<ImageView>(R.id.search_activity_back_icon)
+
+        backIcon.setOnClickListener {
+            finish()
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        }
+
+        searchEditText.addTextChangedListener(textWatcher)
     }
 }
