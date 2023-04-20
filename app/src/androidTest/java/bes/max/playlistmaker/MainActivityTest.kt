6d2dfa831249ef4eb629195751698203677380a3
Max <bespalov.m.9@ya.ru>
2023-04-20 21:21:04 +0300

    refactoring

--- app/src/androidTest/java/bes/max/playlistmaker/MainActivityTest.kt
+++ app/src/androidTest/java/bes/max/playlistmaker/MainActivityTest.kt
@@ -1,8 +1,12 @@
 package bes.max.playlistmaker
 
 import android.content.Intent
+import android.util.Log
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.junit.Test
@@ -22,6 +26,7 @@ class MainActivityTest {
 
     @Test
     fun searchButtonOpensSearchActivity() {
-        val searchIntent = Intent(this, SearchActivity::class.java)
+        //val searchIntent = Intent(this, SearchActivity::class.java)
+        onView(withId(R.id.button_search)).perform(click())
     }
 }
