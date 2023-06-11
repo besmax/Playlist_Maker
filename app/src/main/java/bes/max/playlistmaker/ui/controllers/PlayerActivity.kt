684a9ea22ba2732253ec4c5d4683184d810cbc46
Max <bespalov.m.9@ya.ru>
2023-06-11 14:33:48 +0300

    new vals were added in Track model class

---
+++ app/src/main/java/bes/max/playlistmaker/ui/controllers/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package bes.max.playlistmaker.ui.controllers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bes.max.playlistmaker.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
