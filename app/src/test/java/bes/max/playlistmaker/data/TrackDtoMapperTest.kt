ff89b1f06a46dac7a209bec504e7b4544cbf3501
Max <bespalov.m.9@ya.ru>
2023-07-22 16:42:45 +0300

    test class trackdtomapper was added

---
+++ app/src/test/java/bes/max/playlistmaker/data/TrackDtoMapperTest.kt
@@ -0,0 +1,47 @@
+package bes.max.playlistmaker.data
+
+import bes.max.playlistmaker.data.dto.TrackDto
+import bes.max.playlistmaker.data.mappers.TrackDtoMapper
+import bes.max.playlistmaker.domain.models.Track
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TrackDtoMapperTest {
+
+    private val trackDtoMapperForTest = TrackDtoMapper()
+
+    @Test
+    fun trackDtoToTrackReturnsCorrectTrack() {
+        val trackDto = TrackDto(
+            0L,
+            "trackName",
+            "artistName",
+            98000L,
+            "blablabla/128x128.jpg",
+            "",
+            "2002.12.02",
+            "rock",
+            "Russia",
+            ""
+        )
+
+        val expected = Track(
+            0L,
+            "trackName",
+            "artistName",
+            98000L,
+            "blablabla/128x128.jpg",
+            "",
+            "2002.12.02",
+            "rock",
+            "Russia",
+            "",
+            "01:38",
+            "blablabla/512x512bb.jpg",
+            "2002"
+        )
+
+        assertEquals(expected, trackDtoMapperForTest.trackDtoToTrack(trackDto))
+    }
+}
