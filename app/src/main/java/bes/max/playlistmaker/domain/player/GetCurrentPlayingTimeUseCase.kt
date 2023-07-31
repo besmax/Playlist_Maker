d033c7e39bc447829ab1e1ed357e7f99e67839a4
Max <bespalov.m.9@ya.ru>
2023-07-31 12:03:27 +0300

    use cases in player package were deleted, interactor uses player directly without them

--- app/src/main/java/bes/max/playlistmaker/domain/player/GetCurrentPlayingTimeUseCase.kt
+++
@@ -1,8 +0,0 @@
-package bes.max.playlistmaker.domain.player
-
-import bes.max.playlistmaker.domain.api.Player
-
-class GetCurrentPlayingTimeUseCase(private val player: Player) {
-
-    fun execute() : Int = player.getCurrentPosition()
-}
