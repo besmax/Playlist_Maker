a9ad30bcbfb06e92fadd26421746fe4985d242c6
Max <bespalov.m.9@yandex.ru>
2023-10-31 08:21:13 +0300

    foreign keys were deleted from playlistTrackEntity class to prevent deleting privous row after inserting with the same playlistId

--- app/src/main/java/bes/max/playlistmaker/data/db/entities/PlaylistTrackEntity.kt
+++ app/src/main/java/bes/max/playlistmaker/data/db/entities/PlaylistTrackEntity.kt
@@ -7,20 +7,6 @@ import androidx.room.ForeignKey
 @Entity(
     tableName = "playlist_track_table",
     primaryKeys = ["track_id", "playlist_id"],
-    foreignKeys = [
-        ForeignKey(
-            entity = TrackEntity::class,
-            childColumns = arrayOf("track_id"),
-            parentColumns = arrayOf("track_id"),
-            onDelete = ForeignKey.CASCADE
-        ),
-        ForeignKey(
-            entity = PlaylistEntity::class,
-            childColumns = arrayOf("playlist_id"),
-            parentColumns = arrayOf("playlist_id"),
-            onDelete = ForeignKey.CASCADE
-        ),
-    ]
 )
 data class PlaylistTrackEntity(
 
