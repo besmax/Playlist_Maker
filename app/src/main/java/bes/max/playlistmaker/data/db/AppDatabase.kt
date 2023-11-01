751fc268e2bce59c952d87a90cde5ace0ca78054
Max <bespalov.m.9@ya.ru>
2023-11-01 21:54:56 +0300

    refactoring

--- app/src/main/java/bes/max/playlistmaker/data/db/AppDatabase.kt
+++ app/src/main/java/bes/max/playlistmaker/data/db/AppDatabase.kt
@@ -10,7 +10,7 @@ import bes.max.playlistmaker.data.db.entities.PlaylistTrackEntity
 import bes.max.playlistmaker.data.db.entities.TrackEntity
 
 @Database(
-    version = 2,
+    version = 3,
     entities = [TrackEntity::class, PlaylistEntity::class, PlaylistTrackEntity::class]
 )
 abstract class AppDatabase : RoomDatabase() {
