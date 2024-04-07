669ff893428011aa1d2250f8f5bd9c6dc722357a
Max <bespalov.m.9@ya.ru>
2024-04-07 11:41:16 +0300

    imports were optimized

--- app/src/main/java/bes/max/playlistmaker/data/player/PlayerImpl.kt
+++ app/src/main/java/bes/max/playlistmaker/data/player/PlayerImpl.kt
@@ -9,11 +9,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
