395fcde46545a6c448cbefae39d6040eda5db5c0
bespalovm <bespalovm@radioavionica.ru>
2024-08-13 09:12:52 +0300

    add settings section

--- app/src/main/java/bes/max/playlistmaker/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/bes/max/playlistmaker/presentation/settings/SettingsFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import bes.max.playlistmaker.R
-import bes.max.playlistmaker.presentation.utils.setClickListenersForAllViews
+import bes.max.playlistmaker.ui.theme.PlaylistMakerTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SettingsFragment : Fragment() {
 
     private val viewModel: SettingsViewModel by viewModel()
+    private val settingsViewModel: SettingsViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +25,10 @@ class SettingsFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "Hello world.")
+                val darkTheme by settingsViewModel.isNightModeActive.observeAsState(initial = isSystemInDarkTheme())
+                PlaylistMakerTheme(darkTheme = darkTheme) {
+                    SettingsScreen(viewModel = viewModel)
+                }
             }
         }
     }
@@ -30,37 +36,9 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.settingsScreenSectionShare.setClickListenersForAllViews {
-            viewModel.shareApp(getString(R.string.link_for_app_share))
-        }
-
-        binding.settingsScreenSectionSupport.setClickListenersForAllViews {
-            sendEmailIntent()
-        }
-
-        binding.settingsScreenSectionAgreement.setClickListenersForAllViews {
-            viewModel.openUserAgreement(getString(R.string.link_for_app_share))
-        }
-
-        viewModel.isNightModeActive.observe(viewLifecycleOwner) {
-            binding.settingsScreenThemeSwitcher.isChecked = it
-        }
-
-        binding.settingsScreenThemeSwitcher.setOnCheckedChangeListener { _, checked ->
-            viewModel.setIsNightModeActiveDebounce(checked)
-        }
-
         viewModel.showingToast.observe(viewLifecycleOwner) {
             showToast(it)
         }
-    }
-
-    private fun sendEmailIntent() {
-        viewModel.contactSupport(
-            getString(R.string.email_for_support),
-            getString(R.string.email_theme_for_support),
-            getString(R.string.email_text_for_support)
-        )
     }
 
     private fun showToast(stringRes: Int) {
--- app/src/main/java/bes/max/playlistmaker/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/bes/max/playlistmaker/presentation/settings/SettingsScreen.kt
@@ -1,17 +1,23 @@
 package bes.max.playlistmaker.presentation.settings
 
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import bes.max.playlistmaker.R
 import bes.max.playlistmaker.ui.theme.ysDisplayFamily
 
 
@@ -22,6 +28,35 @@ fun SettingsScreen(
     val showToast by viewModel.showingToast.observeAsState()
     val isNightModeActive by viewModel.isNightModeActive.observeAsState()
 
+    val shareUrl = stringResource(R.string.link_for_app_share)
+
+    SettingSection(
+        onSectionClick = { viewModel.shareApp(shareUrl) },
+        iconResId = R.drawable.ic_share,
+        title = stringResource(id = R.string.share_app)
+    )
+
+    val email = stringResource(R.string.email_for_support)
+    val emailSubject = stringResource(R.string.email_theme_for_support)
+    val emailText = stringResource(R.string.email_text_for_support)
+
+    SettingSection(
+        onSectionClick = {
+            viewModel.contactSupport(email, emailSubject, emailText)
+        },
+        iconResId = R.drawable.ic_support,
+        title = stringResource(id = R.string.contact_support)
+    )
+
+    val agreementUrl = stringResource(R.string.link_for_app_share)
+
+    SettingSection(
+        onSectionClick = {
+            viewModel.openUserAgreement(agreementUrl)
+        },
+        iconResId = R.drawable.ic_support,
+        title = stringResource(id = R.string.contact_support)
+    )
 
 }
 
@@ -36,6 +71,7 @@ fun SettingSection(
         modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 20.dp)
+            .clickable { onSectionClick }
     ) {
 
         Text(
@@ -43,6 +79,12 @@ fun SettingSection(
             fontFamily = ysDisplayFamily,
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp,
+        )
+
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = "settings icon $title",
+            tint = Color.Unspecified
         )
 
     }
