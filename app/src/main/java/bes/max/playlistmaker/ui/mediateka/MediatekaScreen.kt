95b538ade1fabb6b4c9468b8fe6cb7f12567ee5f
bespalovm <bespalovm@radioavionica.ru>
2024-08-18 15:59:37 +0300

    mediateka screen refactoring

--- app/src/main/java/bes/max/playlistmaker/ui/mediateka/MediatekaScreen.kt
+++ app/src/main/java/bes/max/playlistmaker/ui/mediateka/MediatekaScreen.kt
@@ -1,25 +1,40 @@
 package bes.max.playlistmaker.ui.mediateka
 
+import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import bes.max.playlistmaker.R
 import bes.max.playlistmaker.ui.common.Title
 import bes.max.playlistmaker.ui.mediateka.favorite.FavoriteTracksScreen
 import bes.max.playlistmaker.ui.mediateka.playlists.PlaylistsScreen
+import bes.max.playlistmaker.ui.theme.YpBlack
+import bes.max.playlistmaker.ui.theme.ysDisplayFamily
 import kotlinx.coroutines.launch
 
 private const val PAGE_COUNT = 2
@@ -34,42 +49,57 @@ fun MediatekaScreen(
 
     val pagerState = rememberPagerState(pageCount = { PAGE_COUNT })
 
-    val selectedTabIndex = remember { pagerState.currentPage }
+    var selectedTabIndex = pagerState.currentPage
+
+    val tabs = listOf(
+        stringResource(id = R.string.mediateka_screen_tab_title_playlists),
+        stringResource(id = R.string.mediateka_screen_tab_title_favorite_tracks)
+    )
 
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        
+
         Title(text = stringResource(R.string.mediateka))
 
         TabRow(
             selectedTabIndex = selectedTabIndex,
-            modifier = Modifier.fillMaxWidth()
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.background,
+            indicator = {
+                Box(
+                    Modifier
+                        .tabIndicatorOffset(it[selectedTabIndex])
+                        .height(2.dp)
+                        .border(2.dp, MaterialTheme.colorScheme.onBackground)
+                )
+            },
+            modifier = Modifier.fillMaxWidth(),
         ) {
-            Tab(
-                selected = selectedTabIndex == 0,
-                selectedContentColor = MaterialTheme.colorScheme.onBackground,
-                unselectedContentColor = MaterialTheme.colorScheme.onBackground,
-                onClick = {
-                    scope.launch {
-                        pagerState.animateScrollToPage(0)
-                    }
-                },
-                text = { Text(text = stringResource(id = R.string.mediateka_screen_tab_title_my)) },
-            )
 
-            Tab(
-                selected = selectedTabIndex == 1,
-                selectedContentColor = MaterialTheme.colorScheme.primary,
-                unselectedContentColor = MaterialTheme.colorScheme.outline,
-                onClick = {
-                    scope.launch {
-                        pagerState.animateScrollToPage(1)
-                    }
-                },
-                text = { Text(text = stringResource(id = R.string.mediateka_screen_tab_title_favorite_tracks)) },
-            )
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    selected = selectedTabIndex == index,
+                    onClick = {
+                        scope.launch {
+                            selectedTabIndex = index
+                            pagerState.animateScrollToPage(index)
+                            Log.e("AAAAA", "selectedTabIndex=${selectedTabIndex}")
+                        }
+                    },
+                ) {
+                    Text(
+                        text = title,
+                        fontFamily = ysDisplayFamily,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.padding(vertical = 16.dp)
+                    )
+                }
+            }
         }
 
         HorizontalPager(
